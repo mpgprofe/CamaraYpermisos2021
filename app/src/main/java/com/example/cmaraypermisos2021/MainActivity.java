f223629a2c251e9ce00d3a9b7a57c54dbb4b1b0a
Manuel Profesor <mpgprofe@iesflorenciopintado.es>
2021-10-18 15:03:33 +0200

    Primera versión

---
+++ app/src/main/java/com/example/cmaraypermisos2021/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.cmaraypermisos2021;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int VENGO_DE_CAMARA = 1;
+    Button buttonHacerFoto;
+ImageView imageView;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode ==VENGO_DE_CAMARA){
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(bitmap);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        buttonHacerFoto = findViewById(R.id.buttonHacerFoto);
+        imageView = findViewById(R.id.imageView);
+
+        buttonHacerFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                startActivityForResult(intent,VENGO_DE_CAMARA);
+            }
+        });
+    }
+}
